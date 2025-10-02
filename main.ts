4d9bcdc7dabff4b5ab216aafb3182f1bcd628ee8
mercy-brice-kamilindi <mercy.brice.kamilindi@stu.ocsb.ca>
2025-10-02 13:30:42 -0400

    RGB LED

--- main.ts
+++ main.ts
@@ -5,4 +5,65 @@
  * This program ...
 */
 
-basic.showString('Hello, World!')
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+// turn on RGB LED
+input.onButtonPressed(Button.A, function() {
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+
+    // turn on red
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+
+    // turn green on
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+
+    // turn blue on
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+
+    // turn purple on
+   pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+
+    // turn cyan on
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 1)
+    basic.pause(1000)
+
+    // turn yellow on
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    pins.digitalWritePin(DigitalPin.P13, 1)
+    basic.pause(1000)
+
+    // turn white on
+    pins.digitalWritePin(DigitalPin.P15, 1)
+    basic.pause(1000)
+
+    // tuen off
+    pins.digitalWritePin(DigitalPin.P13, 0)
+    pins.digitalWritePin(DigitalPin.P14, 0)
+    pins.digitalWritePin(DigitalPin.P15, 0)
+    
+
+
+
+
+
+    
+
+    
+
+
+
+
+
+   
+    
+})
